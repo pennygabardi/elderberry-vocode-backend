1f11cf6dffcca4aedb3846f9651b1ff133a3331c
Penny Gabardi <pennygabardi@gmail.com>
2025-04-23 12:11:21 -0500

    Initial commit of Vocode browser backend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Voice chat bot running at http://localhost:${PORT}`);
+});
